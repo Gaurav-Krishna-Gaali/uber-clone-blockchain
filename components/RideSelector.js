37f90db5c46f5c1b0ce815de7ecf2d10e809a296
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2022-03-10 20:44:20 +0530

    saniity

---
+++ components/RideSelector.js
@@ -0,0 +1,75 @@
+import UberX from "../assets/rides/uberX.png";
+import UberBlack from "../assets/rides/uberBlack.png";
+import UberBlackSuv from "../assets/rides/uberBlackSuv.png";
+import UberSelect from "../assets/rides/uberSelect.png";
+import UberXL from "../assets/rides/uberXL.png";
+import ethLogo from "../assets/eth-logo.png";
+
+import Image from "next/dist/client/image";
+
+const style = {
+  wrapper: `h-full flex flex-col`,
+  title: `text-gray-500 text-center text-xs py-2 border-b`,
+  carList: `flex flex-col flex-1 overflow-scroll`,
+  car: `flex p-3 m-2 items-center border-2 border-white`,
+  selectedCar: `border-2 border-black flex p-3 m-2 items-center`,
+  carImage: `h-14`,
+  carDetails: `ml-2 flex-1`,
+  service: `font-medium`,
+  time: `text-xs text-blue-500`,
+  priceContainer: `flex items-center`,
+  price: `mr-[-0.8rem]`,
+};
+
+const carList = [
+  { service: "UberX", iconUrl: UberX, priceMultiplier: 1 },
+  { service: "UberBlack", iconUrl: UberBlack, priceMultiplier: 1.5 },
+  { service: "UberBlackSuv", iconUrl: UberBlackSuv, priceMultiplier: 1.5 },
+  { service: "UberSelect", iconUrl: UberSelect, priceMultiplier: 1.5 },
+  { service: "UberXL", iconUrl: UberXL, priceMultiplier: 1.5 },
+];
+const RideSelector = () => {
+  const basePrice = 154877;
+  return (
+    <div className={style.wrapper}>
+      <div className={style.title}>Choose a ride, or swip up for more</div>
+      <div className={style.carList}>
+        {carList.map((car, index) => (
+          <div
+            key={index}
+            // className={`${
+            //   selectedRide.service === car.service
+            //     ? style.selectedCar
+            //     : style.car
+            // }`}
+            onClick={() => {
+              setSelectedRide(car);
+              setPrice(
+                ((basePrice / 10 ** 5) * car.priceMultiplier).toFixed(5)
+              );
+            }}
+          >
+            <Image
+              src={car.iconUrl}
+              className={style.carImage}
+              height={50}
+              width={50}
+            />
+            <div className={style.carDetails}>
+              <div className={style.service}>{car.service}</div>
+              <div className={style.time}>5 min away</div>
+            </div>
+            <div className={style.priceContainer}>
+              <div className={style.price}>
+                {((basePrice / 10 ** 5) * car.priceMultiplier).toFixed(5)}
+              </div>
+              <Image src={ethLogo} height={25} width={40} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RideSelector;
